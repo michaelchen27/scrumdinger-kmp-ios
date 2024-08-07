14e53720a15bfd67190fa51cab4fb049cb4e41d8
Michael Chen <michael.chen@quipper.com>
2024-08-07 16:50:53 +0800

    ScrumTimer

---
+++ Scrumdinger/Views/MeetingView.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.theme.mainColor)
+            
+            VStack {         
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
+                                  secondsRemaining: scrumTimer.secondsRemaining,
+                                  theme: scrum.theme)
+                
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                
+                MeetingFooterView(
+                    speakers: scrumTimer.speakers, skipAction: {
+                        scrumTimer.skipSpeaker()
+                    })
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+
+    }
+}
+
+#Preview {
+    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+}
